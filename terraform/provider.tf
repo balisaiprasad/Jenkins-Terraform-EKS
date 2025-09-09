b3671f854ffaa48345148512c17f0c60decb875b
Bali Saiprasad <124686217+balisaiprasad@users.noreply.github.com>
2025-09-09 23:06:28 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,7 +5,7 @@ locals {
   azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  
   tags = {
     Example = local.name
   }
